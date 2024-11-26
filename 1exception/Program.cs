0dbaa074613bf56847c2eda9d45e63c1de54de47
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-11-26 12:32:23 +0200

    add class DoFiles, static fields, method ReadFiles

---
+++ 1exception/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1exception
+{
+    class DoFiles
+    {
+        private static int sum = 0;
+        private static int count = 0;
+        public static void ReadFiles(List<string> files, List<string> nofiles, List<string> badData, List<string> overflowData)
+        {
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
